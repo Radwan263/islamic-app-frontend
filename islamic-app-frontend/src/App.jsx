8397f4157a3eb73298ddd02d2a19299209f65e8b
Radwan226 <rn2632000@gmail.com>
2025-09-05 07:48:06 +0300

    Update files

--- islamic-app-frontend/src/App.jsx
+++ islamic-app-frontend/src/App.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-// ... (باقي استيراد المكونات كما هو)
+// استيراد المكونات الموجودة فقط
 import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
 import QuranSection from './components/QuranSection';
-// ...
+import HomePage from './pages/HomePage';
+
+// تعليق استيراد المكونات غير الموجودة حاليًا
+// import HadithSection from './components/HadithSection';
+// import AzkarSection from './components/AzkarSection';
+// import DuasSection from './components/DuasSection';
+// import SadaqaSection from './components/SadaqaSection';
+// import TasbihSection from './components/TasbihSection';
+// import PrayerTimesSection from './components/PrayerTimesSection';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* الصفحة الرئيسية الآن لا تحتوي على Navbar */}
+        {/* 1. الصفحة الرئيسية (موجودة وتعمل) */}
         <Route path="/" element={<HomePage />} />
 
-        {/* باقي الصفحات تحتوي على Navbar */}
+        {/* 2. صفحة القرآن (موجودة وتعمل) */}
         <Route path="/quran" element={<><Navbar /><QuranSection /></>} />
+
+        {/* تعليق المسارات للمكونات غير الموجودة */}
+        {/* 
         <Route path="/hadith" element={<><Navbar /><HadithSection /></>} />
         <Route path="/azkar" element={<><Navbar /><AzkarSection /></>} />
         <Route path="/duas" element={<><Navbar /><DuasSection /></>} />
         <Route path="/sadaqa" element={<><Navbar /><SadaqaSection /></>} />
         <Route path="/tasbih" element={<><Navbar /><TasbihSection /></>} />
         <Route path="/prayer-times" element={<><Navbar /><PrayerTimesSection /></>} />
+        */}
       </Routes>
     </Router>
   );
